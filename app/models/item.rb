a2a2152f9bf03d6d9d97ed2f99a28e485967e9f2
fang duan <df1228@gmail.com>
2021-02-19 16:29:45 +0800

    activejob

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,14 +7,22 @@ class Item < ApplicationRecord
   after_create :fix_image_urls
 
   # FILETYPES = ["PSD", "XD", "AI", "Sketch"]
-
   # Item.last.update_column(:cover, Item.last.cover.service_url)
   def fix_image_urls
-    # https://stackoverflow.com/questions/51110789/activestorage-service-url-rails-blob-path-cannot-generate-full-url-when-not-u
-    ActiveStorage::Current.set(host: "http://localhost:3000") do
-      self.update_column(:cover, self.cover.service_url)
-      self.update_column(:detail, self.detail.service_url)
-      self.update_column(:url, self.url.service_url)
+    if Rails.env.development?
+      ActiveStorage::Current.set(host: "http://localhost:3000") do
+        self.update_column(:cover, self.cover.service_url)
+        self.update_column(:detail, self.detail.service_url)
+        self.update_column(:url, self.url.service_url)
+      end
+    else
+      # https://stackoverflow.com/questions/51110789/activestorage-service-url-rails-blob-path-cannot-generate-full-url-when-not-u
+      ActiveStorage::Current.set(host: "http://assets.2mui.cn") do
+        self.update_column(:cover, self.cover.service_url)
+        self.update_column(:detail, self.detail.service_url)
+        self.update_column(:url, self.url.service_url)
+      end
     end
+    
   end
 end
