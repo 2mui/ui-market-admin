343d6e1b0f8d13be3b0703387ba100ddf53b0928
fang duan <df1228@gmail.com>
2021-03-01 11:56:44 +0800

    Fix

--- app/jobs/banner_job.rb
+++ app/jobs/banner_job.rb
@@ -4,7 +4,7 @@ class BannerJob < ApplicationJob
   queue_as :default
 
   def perform(banner_id)
-    item = Banner.find(banner_id)
+    banner = Banner.find(banner_id)
     if Rails.env.development?
       url = "http://localhost:5000"
     else
@@ -12,7 +12,7 @@ class BannerJob < ApplicationJob
     end
 
     ActiveStorage::Current.set(host: url) do
-      if banner.image.attached? && item.image.image?
+      if banner.image.attached? && banner.image.image?
         banner.update_column(:image, banner.image.variant(resize_to_limit: [nil,600]).processed.url) 
       end
     end
